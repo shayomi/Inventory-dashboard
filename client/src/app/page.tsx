fcc27837ed776fa3727e301bc41954b5378978a7
Sayo Adegoroye <steve.legion94@gmail.com>
2025-01-19 21:48:09 +0000

    initial commit

---
+++ client/src/app/page.tsx
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import Image from "next/image";
+import Dashboard from "./dashboard/page";
+
+export default function Home() {
+  return <Dashboard />;
+}
