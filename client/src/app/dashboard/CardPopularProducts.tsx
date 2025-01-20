83067f77193e41ebc3df1046fef7a30ba6b49ba5
Sayo Adegoroye <steve.legion94@gmail.com>
2025-01-20 00:55:15 +0000

    made changes to aqs links

--- client/src/app/dashboard/CardPopularProducts.tsx
+++ client/src/app/dashboard/CardPopularProducts.tsx
@@ -25,7 +25,7 @@ const CardPopularProducts = () => {
               >
                 <div className="flex items-center gap-3">
                   <Image
-                    src={`https://s3-inventorydashboard.s3.eu-west-2.amazonaws.com/product$.png${
+                    src={`https://s3-inventorydashboard.s3.us-east-2.amazonaws.com/product${
                       Math.floor(Math.random() * 3) + 1
                     }.png`}
                     alt={product.name}
