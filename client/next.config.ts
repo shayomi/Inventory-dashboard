2bd5b153b28c46831ad65bed64ac3119e529cb19
Sayo Adegoroye <steve.legion94@gmail.com>
2025-01-20 13:12:03 +0000

    fixed s3 bucket error

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,15 +1,21 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "s3-inventorydashboard.s3.eu-west-2.amazonaws.com",
+        hostname: "s3-inventorydashboard.s3.eu-west-2.amazonaws.com", // For your product images
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "s3-inventorydashboard.s3.us-west-2.amazonaws.com", // For other S3 images (if applicable)
         port: "",
         pathname: "/**",
       },
     ],
   },
 };
+
 export default nextConfig;
